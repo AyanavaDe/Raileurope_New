4a11391288549deaa2b5a50b21b1cb884733a632
Ayanava <Ayanava@Ayanava>
2025-04-13 14:54:08 +0530

    Fourth commit.

--- Automation/src/Trezy_Generic/Base_constant.java
+++ Automation/src/Trezy_Generic/Base_constant.java
@@ -3,9 +3,9 @@ package Trezy_Generic;
 public interface Base_constant 
 {
 	String gecko_key="webdriver.gecko.driver";
-	String gecko_value="./softwares/geckodriver.exe";
+	String gecko_value="./Softwares/geckodriver.exe";
 	String chrome_key="webdriver.chrome.driver";
-	String chrome_value="./softwares/chromedriver.exe";
+	String chrome_value="./Softwares/chromedriver.exe";
 	String url="https://trezy.pages.dev/";
 	int time=80;
 	int time1=10;
